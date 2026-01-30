9c7cf065834455bef18f42d017c91243c4aa16c8
AracnidKnight <jjifernandezcubillos@gmail.com>
2026-01-30 11:45:08 -0300

    Added basic dash

--- scenes/objects/player/player.gd
+++ scenes/objects/player/player.gd
@@ -1,12 +1,26 @@
 extends CharacterBody2D
 
+var DASH_SPEED : float = 2500.0;
+
 var gravity = 1500.0;
 
+var h_direction : float = 0.0;
 var h_speed : float = 256.0;
 var v_speed : float = 0.0;
 
 func _process(delta):
-	var h_direction : float = Input.get_axis("go_left", "go_right");
+	# Dash state overwrites all movement.
+	if $DashTimer.time_left > 0.0:
+		velocity = Vector2(DASH_SPEED * h_direction, 0.0);
+		move_and_slide();
+		return;
+	
+	h_direction = Input.get_axis("go_left", "go_right");
+	
+	if Input.is_action_just_pressed("dash"):
+		h_direction = sign(h_direction);
+		$DashTimer.start();
+		return;
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		v_speed = -800.0;
