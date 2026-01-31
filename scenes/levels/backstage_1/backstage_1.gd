c7bef87ca6970cb7fc3e55361d1b000dc8f36e8f
AracnidKnight <jjifernandezcubillos@gmail.com>
2026-01-31 17:20:49 -0300

    Added small dash tutorial

--- scenes/levels/backstage_1/backstage_1.gd
+++ scenes/levels/backstage_1/backstage_1.gd
@@ -1,5 +1,10 @@
 extends Node
 
+enum TUTORIAL_TYPE {NONE, DASH};
+
+var current_tutorial : TUTORIAL_TYPE = TUTORIAL_TYPE.NONE;
+var dash_tween : Tween = null;
+
 func _ready():
 	$Player/CameraTransform.remote_path = $Camera2D.get_path();
 	
@@ -12,22 +17,65 @@ func _ready():
 		pass;
 	pass;
 
+func _process(_delta):
+	if current_tutorial == TUTORIAL_TYPE.DASH:
+		if Input.is_action_just_pressed("dash"):
+			end_dash_tutorial();
+	pass;
+
 func _laught_circle_attack():
 	var laught_scene := preload("res://scenes/boss_attacks/laugh_attack/laugh_attack.tscn");
-	var attack_direction = Vector2.UP * 800.0;
+	var attack_direction = Vector2.UP * 600.0;
 	
-	for i in range(5):
+	for i in range(3):
 		var laught = laught_scene.instantiate();
 		
 		laught.position = $Player.global_position + attack_direction.rotated(TAU / 5.0 * i);
 		laught.objective = $Player;
-		laught.lifetime = 800.0 / laught.speed * 1.5;
+		laught.lifetime = 600.0 / laught.speed * 1.5;
 		
 		add_child(laught);
 	
 	pass;
 
+func spawn_hand():
+	var hand_scene := preload("res://scenes/boss_attacks/hand_fall_attack/hand_fall_attack.tscn");
+	var hand = hand_scene.instantiate();
+	hand.position.x = $Player.global_position.x;
+	hand.position.y = $Player.global_position.y - 700.0;
+	hand.fall_speed = 3200.0;
+	add_child(hand);
+	pass;
+
+func spawn_dash_tutorial():
+	current_tutorial = TUTORIAL_TYPE.DASH;
+	dash_tween = create_tween();
+	
+	dash_tween.tween_callback(
+		func():
+			spawn_hand();
+			Engine.time_scale = 0.1;
+			$CanvasLayer/ExtraGUI/DashTutorialLabel.show();
+	);
+	dash_tween.tween_interval(0.4);
+	dash_tween.tween_callback(
+		func():
+			Engine.time_scale = 1.0;
+	);
+	
+	dash_tween.play();
+	pass;
+
+func end_dash_tutorial():
+	dash_tween.kill();
+	Engine.time_scale = 1.0;
+	$CanvasLayer/ExtraGUI/DashTutorialLabel.hide();
+	pass;
 
 func _on_hand_fall_timer_timeout():
-	_laught_circle_attack();
+	pass;
+
+func _on_dash_tutorial_area_area_entered(_area):
+	spawn_dash_tutorial();
+	$DashTutorialArea.queue_free();
 	pass;
